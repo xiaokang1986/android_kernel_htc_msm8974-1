30758ba7cb2ef7c181730f63ab645ad555a72e52
Paul Keith <javelinanddart@gmail.com>
2015-05-03 00:48:31 -0500

    Add SmartMAX M8 Defaults
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- drivers/cpufreq/cpufreq_smartmax_eps.c
+++ drivers/cpufreq/cpufreq_smartmax_eps.c
@@ -131,6 +131,24 @@ extern int tegra_input_boost (struct cpufreq_policy *policy,
 #define DEFAULT_IGNORE_NICE 1
 #endif
 
+#ifdef CONFIG_CPU_FREQ_GOV_SMARTMAX_M8
+#define DEFAULT_SUSPEND_IDEAL_FREQ 422400
+#define DEFAULT_AWAKE_IDEAL_FREQ 7296000
+#define DEFAULT_RAMP_UP_STEP 768000
+#define DEFAULT_RAMP_DOWN_STEP 768000
+#define DEFAULT_MAX_CPU_LOAD 70
+#define DEFAULT_MIN_CPU_LOAD 40
+#define DEFAULT_UP_RATE 30000
+#define DEFAULT_DOWN_RATE 60000
+#define DEFAULT_SAMPLING_RATE 30000
+#define DEFAULT_INPUT_BOOST_DURATION 90000
+#define DEFAULT_TOUCH_POKE_FREQ 1267000
+#define DEFAULT_BOOST_FREQ 1267000
+#define DEFAULT_IO_IS_BUSY 0
+#define DEFAULT_IGNORE_NICE 1
+#endif
+
+
 static unsigned int suspend_ideal_freq;
 static unsigned int awake_ideal_freq;
 /*
