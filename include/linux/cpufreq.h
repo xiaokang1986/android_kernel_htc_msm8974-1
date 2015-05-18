820e0ad831795b3f5c34a6c00551aa4d7a0caf6a
Paul Keith <javelinanddart@gmail.com>
2015-05-18 12:02:34 -0500

    Add Samsung CPU Governor
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -446,6 +446,9 @@ extern struct cpufreq_governor cpufreq_gov_tripndroid;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELEMENTALX)
 extern struct cpufreq_governor cpufreq_gov_elementalx;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_elementalx)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAMSUNG)
+extern struct cpufreq_governor cpufreq_gov_samsung;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_samsung)
 #endif
 
 
