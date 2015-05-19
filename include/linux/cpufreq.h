4d0030363d34ce83d041ad212eac92dd8628e3a2
Paul Keith <javelinanddart@gmail.com>
2015-05-19 10:55:05 -0500

    Squashed Revert of Samsung CPU Governor Revert "cpufreq_samsung: Forward port to Linux 3.4"
    This reverts commit db2d59a854e9ac1906fc83acf8e6edc71897102b.
    
    Revert "Add Samsung CPU Governor"
    
    This reverts commit 820e0ad831795b3f5c34a6c00551aa4d7a0caf6a.
    
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -446,9 +446,6 @@ extern struct cpufreq_governor cpufreq_gov_tripndroid;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELEMENTALX)
 extern struct cpufreq_governor cpufreq_gov_elementalx;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_elementalx)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAMSUNG)
-extern struct cpufreq_governor cpufreq_gov_samsung;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_samsung)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAVAGEDZEN)
 extern struct cpufreq_governor cpufreq_gov_savagedzen;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_savagedzen)
